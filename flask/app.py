577d485b31f31943f356eef237b00a4da33ef7b9
Mitchell Tishmack <git@mitchty.net>
2021-02-25 11:18:31 -0600

    v1

--- flask/app.py
+++ flask/app.py
@@ -2,16 +2,20 @@ from flask import Flask
 import os
 from redis import Redis
 
-redis = Redis(host=os.getenv('REDIS_HOST', 'localhost'),
-              port=os.getenv('REDIS_PORT', 6379))
+master = Redis(host='redis-master', port=6379)
+slave = Redis(host='redis-slave', port=6379)
 app = Flask(__name__)
 
 # This is not intended to be particuarly interesting as an app
 @app.route('/')
-def hello():
-    redis.incr('hits')
-    hits = int(redis.get('hits'))
-    return f"hits counter: {hits}"
+def hits():
+    master.incr('hits')
+    hits = int(slave.get('hits'))
+    print(f"hits: {hits}")
+    return f"Hits counter: {hits}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    print("main()")
+    master.set('hits', 0)
+    print("run()")
+    app.run(host='0.0.0.0', port=80)
